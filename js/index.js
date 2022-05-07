7d838c494908f93a3c991c4b4e8c2ee95789c94a
Isaiah <ijandicos90@gmail.com>
2022-05-06 22:57:38 -0400

    Completed Assignment

--- js/index.js
+++ js/index.js
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
  async function gitUserSelect(userName) {
  try {const response = await fetch(`https://api.github.com/search/users?q=${userName}`)
  const data = await response.json();
- console.log(data)
+ 
  
  data.items.forEach(renderData)
-    
+  console.log(data.items)  
  }
 
 catch(error){ console.error(error) }
@@ -28,30 +28,48 @@ catch(error){ console.error(error) }
 function renderData(user){
 	
 const userList = document.getElementById('user-list')
+const reposList = document.getElementById('repos-list')
+const repoListItem = document.createElement("li")
 const userListItem = document.createElement("li")
 const userLogin = document.createElement('p')
 const userPicPara = document.createElement('p')
 const userLinkPara = document. createElement('p')
 const userAvatar = document.createElement('img')
 const userLink = document.createElement('a')
-userLink.href = `${user.html_url}`
-userLink.textContent = `${user.html_url}`
-userLinkPara.appendChild(userLink)
-userPicPara.appendChild(userAvatar)
-userAvatar.src = `${user.avatar_url}`
-userAvatar.alt = "mob at a hundred percent"
-userAvatar.style.cssText = "height: 25%; display: block; margin-left: auto; margin-right: auto"
-userLogin.innerText = `${user.login}`
-userLogin.style.textAlign = "center"
-userLinkPara.style.textAlign = "center"
-userList.append(userListItem)
-userListItem.previousElementSibling.remove()
-userListItem.append(userLogin, userPicPara, userLinkPara)
-console.log(userList)
+    userLink.href = `${user.html_url}`
+    userLink.textContent = `${user.html_url}`
+    userLinkPara.appendChild(userLink)
+    userPicPara.appendChild(userAvatar)
+    userAvatar.src = `${user.avatar_url}`
+    userAvatar.alt = "mob at a hundred percent"
+    userAvatar.style.cssText = "height: 25%; display: block; margin-left: auto; margin-right: auto"
+    userLogin.innerText = `${user.login}`
+    userLogin.style.textAlign = "center"
+    userLinkPara.style.textAlign = "center"
+    userList.append(userListItem)
+    reposList.append(repoListItem)
+    userListItem.previousElementSibling.remove()
+    userListItem.append(userLogin, userPicPara, userLinkPara)
+        const btn = document.createElement('button')
+        btn.innerText = "Get Repositories"
+        btn.addEventListener('click', handleRepo(`${user.login}`))
+        userLogin.append(btn)
+        console.log(userList)
 }
 
 
+function handleRepo(userName) {
+    fetch(`https://api.github.com/users/${userName}/repos`)
+    .then(res => res.json())
+    .then((repoData) => {
+        for(const data of repoData) {
+    console.log(data.html_url)
+        }
+  
+    })
+    .catch(err => console.log(err))
 
+}
 
 
 
